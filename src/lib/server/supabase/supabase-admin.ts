9d163ec05a82a9bef53f5cfb2941995818e05072
jaxnalia <jacksendall@gmail.com>
2024-05-31 04:11:15 +0100

    0

--- src/lib/server/supabase/supabase-admin.ts
+++ src/lib/server/supabase/supabase-admin.ts
@@ -1,7 +1,7 @@
 import type { Database } from "$lib/types_db"
 import { createClient } from "@supabase/supabase-js";
 import { PUBLIC_SUPABASE_URL } from "$env/static/public";
-import { SUPABASE_SERVICE_ROLE_KEY } from "$env/dynamic/private"
+import { SUPABASE_SERVICE_ROLE_KEY } from "$env/static/private"
 
 export const supabaseAdmin = createClient<Database>(
 	PUBLIC_SUPABASE_URL,
