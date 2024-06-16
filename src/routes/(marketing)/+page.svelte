24cd2bced320eee793ed66e6ba137a183d8e2754
jaxnalia <jacksendall@gmail.com>
2024-06-16 18:44:45 +0100

    typo

--- src/routes/(marketing)/+page.svelte
+++ src/routes/(marketing)/+page.svelte
@@ -12,7 +12,7 @@
   <title>{WebsiteName}</title>
   <meta
     name="og:description"
-    content="#1 Trading Course on the Web &#9889; Supply and Demand Trading U+26A1 Liquidity Scalping 🎓 We teach you everything ou need to know to become a profitable trader in 90 days"
+    content="#1 Trading Course on the Web &#9889; Supply and Demand Trading U+26A1 Liquidity Scalping 🎓 We teach you everything you need to know to become a profitable trader in 90 days"
   />
 </svelte:head>
 
